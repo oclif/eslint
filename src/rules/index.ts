4f0d746f0ca58c60fff254b6dfa0d45ca621c9c1
Moulik Aggarwal <qwertymoulik@gmail.com>
2019-07-21 04:01:47 +0530

    Added multiple-flags

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,5 +1,7 @@
 import commandClassVariable from './command-class-variables';
+import noMultipleFlags from './no-multiple-flags';
 
 export default {
   'command-class-variables': commandClassVariable,
+  'no-multiple-flags': noMultipleFlags
 }
