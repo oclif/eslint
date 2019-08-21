0ee85206e2d3c6b5be05109838d96175dd15adc7
Moulik Aggarwal <qwertymoulik@gmail.com>
2019-08-21 11:40:02 +0530

    Added new rule no-duplicate-args

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,7 +1,9 @@
 import commandClassVariable from './command-class-variables';
 import noMultipleFlags from './no-multiple-flags';
+import noMultipleArgs from './no-duplicate-args';
 
 export default {
   'command-class-variables': commandClassVariable,
-  'no-multiple-flags': noMultipleFlags
+  'no-multiple-flags': noMultipleFlags,
+  'no-duplicate-args': noMultipleArgs
 }
---
+++ src/rules/no-duplicate-args.ts
@@ -0,0 +1,66 @@
+import {
+    AST_NODE_TYPES,
+    TSESTree    
+} from '@typescript-eslint/experimental-utils';
+import { createRule } from '../util';
+
+export default createRule({
+    name: 'no-duplicate-args',
+    meta: {
+        type: 'problem',
+        docs: {
+            description: '',
+            category: 'Possible Errors',
+            recommended: false,
+        },
+        schema: [],
+        messages: {
+            'errorMultipleArgs': `Avoid using same name arguments {{ name }}`,
+        }
+    },
+    defaultOptions: [],
+    create(context) {
+        return{
+            'ClassDeclaration, ClassExpression'(node: TSESTree.ClassDeclaration | 
+                TSESTree.ClassExpression) {
+                    const sourceCode = context.getSourceCode();
+                    const properties = node.body.body;
+                    let flagsNameVal: Array<string> = [];
+
+                    for(let element of properties){
+                        if(element.type === AST_NODE_TYPES.ClassProperty){
+                            let name = sourceCode.text.slice(...element.key.range);
+                            if(name === 'args' && element.value.type === AST_NODE_TYPES.ArrayExpression){
+                                let values = element.value.elements;
+                                for(let objEle of values){
+                                    if(objEle.type === AST_NODE_TYPES.ObjectExpression){
+                                        let objProp = objEle.properties;
+                                        for(let props of objProp){
+                                            if(props.type === AST_NODE_TYPES.Property){
+                                                let propName = sourceCode.text.slice(...props.key.range);
+                                                let propVal = sourceCode.text.slice(...props.value.range);
+                                                if(propName === 'name'){
+                                                    if(!flagsNameVal.includes(propVal))
+                                                    flagsNameVal.push(propVal);
+                                                    else {
+                                                        context.report({
+                                                            node,
+                                                            messageId: 'errorMultipleArgs',
+                                                            data: {
+                                                                name: propVal,
+                                                            }
+                                                        });
+                                                    }
+                                                    break;
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+});
