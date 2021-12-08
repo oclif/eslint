9880c1df8ca611d9e0f381f6ed953e20dd61e8b5
Mike Donnalley <mdonnalley@salesforce.com>
2021-12-08 14:53:11 -0700

    chore: replace instances of master with main [skip ci]

--- src/util/index.ts
+++ src/util/index.ts
@@ -2,5 +2,5 @@ import { ESLintUtils } from '@typescript-eslint/experimental-utils';
 
 export const createRule = ESLintUtils.RuleCreator(
   name =>
-    `https://github.com/moulikcipherx/eslint-plugin-oclif/blob/master/docs/rules/${name}.md`,
+    `https://github.com/moulikcipherx/eslint-plugin-oclif/blob/main/docs/rules/${name}.md`,
 );
